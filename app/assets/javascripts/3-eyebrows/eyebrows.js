502a8bb5fb202c7a53c8d2351aa4e1cb521a1b55
Sarah French <sarah@sarahfrench.co.uk>
2019-08-31 21:43:11 +0100

    The mega drop event listener function was split up into smaller chunks

--- app/assets/javascripts/3-eyebrows/eyebrows.js
+++ app/assets/javascripts/3-eyebrows/eyebrows.js
@@ -43,23 +43,59 @@ function angryShake(){
   }, 1000);
 }
 
-document.addEventListener("drop", function(event){
-  var data = event.dataTransfer.getData("Text");
-  if (data === 'frag' && event.target.id === 'hugo'){
-    happyObject = document.getElementsByClassName("happy")
-    Object.keys(happyObject).forEach(function (key){
-      happyObject[key].setAttribute('opacity', 1);
-    });
+function makeHugoSVGHappy(){
+  happyObject = document.getElementsByClassName("happy")
+  Object.keys(happyObject).forEach(function (key){
+    happyObject[key].setAttribute('opacity', 1);
+  });
 
-    unHappyObject = document.getElementsByClassName("unhappy")
-    Object.keys(unHappyObject).forEach(function (key){
-      unHappyObject[key].setAttribute('opacity', 0);
-    });
+  unHappyObject = document.getElementsByClassName("unhappy")
+  Object.keys(unHappyObject).forEach(function (key){
+    unHappyObject[key].setAttribute('opacity', 0);
+  });
+}
 
-    event.target.parentNode.appendChild(document.getElementById('frag'))
+function moveFragrance(){
+  let hugoContainer = document.getElementById('hugo-container')
+  hugoContainer.appendChild(document.getElementById('favourite'))
+}
 
-  } else if (data != 'frag' && event.target.id === 'hugo'){
-    angryShake();
+function showHeartRings(){
+  let favourite = document.getElementById('favourite');
+  let innerRing = document.getElementById('ring--inner');
+  let outerRing = document.getElementById('ring--outer');
+
+  favourite.setAttribute('overflow', 'show');
+
+  //using sequential setTimeouts to change opacity as can't do multiple webkit CSS animations on the same element (?)
+  setTimeout(function(){
+    innerRing.setAttribute('opacity', 0.25);
+    outerRing.setAttribute('opacity', 0.25);
+  },500);
+  setTimeout(function(){
+    innerRing.setAttribute('opacity', 0.5);
+    outerRing.setAttribute('opacity', 0.5);
+  },700);
+  setTimeout(function(){
+    innerRing.setAttribute('opacity', 0.75);
+    outerRing.setAttribute('opacity', 0.75);
+  },800);
+  setTimeout(function(){
+    innerRing.setAttribute('opacity', 1);
+    outerRing.setAttribute('opacity', 1);
+  },900);
   }
 
+document.addEventListener("drop", function(event){
+  if (!isHugoHappy){
+    var data = event.dataTransfer.getData("Text");
+    if (data === 'favourite' && event.target.id === 'hugo'){
+      makeHugoSVGHappy();
+      moveFragrance();
+      showHeartRings();
+      isHugoHappy = true;
+    } else if (data != 'favourite' && event.target.id === 'hugo'){
+      angryShake();
+    }
+  }
 });
