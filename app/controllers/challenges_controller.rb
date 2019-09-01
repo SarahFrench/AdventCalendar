042b9179e042784569cedda64db89bd8b8816902
Sarah French <sarah@sarahfrench.co.uk>
2019-09-01 10:05:37 +0100

    Added alternative controller to allow easy development

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -7,9 +7,20 @@ class ChallengesController < ApplicationController
     4 => 'vue'
   }
 
+# final version of controller that responds to what day it is
+  # def show
+  #   day_of_month = params[:id].to_i
+  #   if day_of_month < (Time.now.day + 1)
+  #     render "#{day_of_month}-#{CHALLENGES[day_of_month]}"
+  #   else
+  #     redirect_to root_path
+  #   end
+  # end
+
+#version for development - allow access to all
   def show
     day_of_month = params[:id].to_i
-    if day_of_month < (Time.now.day + 1)
+    if day_of_month < (30)
       render "#{day_of_month}-#{CHALLENGES[day_of_month]}"
     else
       redirect_to root_path
