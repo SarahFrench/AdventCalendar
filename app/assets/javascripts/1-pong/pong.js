67e06bef38a818f7d6a0db6d1a881f63dbcb315f
Sarah French <sarah@sarahfrench.co.uk>
2019-10-12 20:56:59 +0100

    Wrap JS in if statement checking if page is correct

--- app/assets/javascripts/1-pong/pong.js
+++ app/assets/javascripts/1-pong/pong.js
@@ -1,171 +1,174 @@
-document.addEventListener("DOMContentLoaded", function() {
+if(/days\/1/.test(window.location.href)){
 
-  let page = document.getElementsByTagName("body")[0].getAttribute('data-page');
+  document.addEventListener("turbolinks:load", function() {
 
-  class Vector {
-    constructor(x=0, y=0){
-      this.x = x;
-      this.y = y;
+    let page = document.getElementsByTagName("body")[0].getAttribute('data-page');
+
+    class Vector {
+      constructor(x=0, y=0){
+        this.x = x;
+        this.y = y;
+      }
+
+      get len(){
+        return Math.sqrt(this.x*this.x + this.y * this.y);
+      }
+
+      set len(value){
+        const fact = value/this.len
+        this.x *= fact
+        this.y *= fact
+      }
     }
 
-    get len(){
-      return Math.sqrt(this.x*this.x + this.y * this.y);
+    class Rectange{
+      constructor(w,h){
+        this.pos = new Vector;
+        this.size = new Vector(w,h);
+      }
+
+      get left(){
+        return this.pos.x - this.size.x/2
+      }
+
+      get right(){
+        return this.pos.x + this.size.x/2
+      }
+
+      get top(){
+        return this.pos.y - this.size.y/2
+      }
+
+      get bottom(){
+        return this.pos.y + this.size.y/2
+      }
     }
 
-    set len(value){
-      const fact = value/this.len
-      this.x *= fact
-      this.y *= fact
-    }
-  }
-
-  class Rectange{
-    constructor(w,h){
-      this.pos = new Vector;
-      this.size = new Vector(w,h);
+    class Ball extends Rectange {
+      constructor(){
+        super(10,10);
+        this.vel = new Vector;
+      }
     }
 
-    get left(){
-      return this.pos.x - this.size.x/2
+    class Player extends Rectange {
+      constructor(){
+        super(20,100);
+        this.score = 0;
+      }
     }
 
-    get right(){
-      return this.pos.x + this.size.x/2
-    }
+    class Pong {
+      constructor(canvas){
+        this._canvas = canvas;
+        this._context = canvas.getContext("2d");
 
-    get top(){
-      return this.pos.y - this.size.y/2
-    }
+        this.ball = new Ball;
 
-    get bottom(){
-      return this.pos.y + this.size.y/2
-    }
-  }
+        this.players = [
+          new Player,
+          new Player,
+        ]
 
-  class Ball extends Rectange {
-    constructor(){
-      super(10,10);
-      this.vel = new Vector;
-    }
-  }
+        this.players[0].pos.x = 40;
+        this.players[1].pos.x = canvas.width-40;
+        this.players.forEach(player => {
+          player.pos.y = this._canvas.height/2
+        })
 
-  class Player extends Rectange {
-    constructor(){
-      super(20,100);
-      this.score = 0;
-    }
-  }
+        let lastTime;
 
-  class Pong {
-    constructor(canvas){
-      this._canvas = canvas;
-      this._context = canvas.getContext("2d");
+        const callback = (milliseconds) => {
+          if(lastTime){
+            this.update((milliseconds - lastTime)/1000)
+          }
+          lastTime = milliseconds;
+          requestAnimationFrame(callback);
+        };
 
-      this.ball = new Ball;
+        callback();
+        this.reset();
+      }
 
-      this.players = [
-        new Player,
-        new Player,
-      ]
-
-      this.players[0].pos.x = 40;
-      this.players[1].pos.x = canvas.width-40;
-      this.players.forEach(player => {
-        player.pos.y = this._canvas.height/2
-      })
-
-      let lastTime;
-
-      const callback = (milliseconds) => {
-        if(lastTime){
-          this.update((milliseconds - lastTime)/1000)
+      collide(player, ball){
+        if(player.left < ball.right && player.right > ball.left && player.top < ball.bottom && player.bottom > ball.top){
+          const len = ball.vel.len;
+          ball.vel.x = -ball.vel.x;
+          console.log('vel x: ' + ball.vel.x);
+          ball.vel.y = -ball.vel.y;
+          console.log('vel y: ' + ball.vel.y);
+          ball.vel.y += 300 * (Math.random() - 0.5);
+          ball.vel.len = len * 1.05
+          console.log('len: ' + ball.vel.len);
         }
-        lastTime = milliseconds;
-        requestAnimationFrame(callback);
+      }
+
+      drawRect(rectangle){
+        this._context.fillStyle = '#FFF';
+        this._context.fillRect(rectangle.left, rectangle.top,
+                     rectangle.size.x, rectangle.size.y)
+      }
+
+      draw(){
+        this._context.fillStyle = '#000';
+        this._context.fillRect(0,0,this._canvas.width, this._canvas.height)
+
+        this.drawRect(this.ball)
+        this.players.forEach( player => {
+          this.drawRect(player)
+        })
+      }
+
+      reset(){
+        this.ball.pos.x = this._canvas.width/2;
+        this.ball.pos.y = this._canvas.height/2;
+        this.ball.vel.x = 0;
+        this.ball.vel.y = 0;
+      }
+
+      start(){
+        if(this.ball.vel.x === 0 && this.ball.vel.y === 0){
+          this.ball.vel.x = 300 * (Math.random() > 0.5 ? 1: -1);
+          this.ball.vel.y = 300 * (Math.random() > 0.5 ? 1: -1);
+          this.ball.vel.len = 200;
+        }
+      }
+
+      update(dt){
+        this.ball.pos.x += this.ball.vel.x * dt;
+        this.ball.pos.y += this.ball.vel.y * dt;
+        // console.log(this.ball.pos.x, this.ball.pos.y);
+
+        if (this.ball.left < 0 || this.ball.right > this._canvas.width){
+          const playerId = this.ball.vel.x < 0 | 0
+          this.players[playerId].score++
+          this.reset()
+        }
+
+        if (this.ball.top < 0 || this.ball.bottom > this._canvas.height){
+          this.ball.vel.y = -this.ball.vel.y
+        }
+
+        this.players[1].pos.y = this.ball.pos.y
+        this.players.forEach( player => { this.collide(player, this.ball)})
+        this.draw();
+      }
+    }
+
+    const canvas = document.getElementById('pong');
+    const pong = new Pong(canvas);
+
+    canvas.addEventListener('mousemove', event => {
+      if (event.y < 50){
+        pong.players[0].pos.y = 50;
+      } else if ( event.y > canvas.height-50 ) {
+        pong.players[0].pos.y = canvas.height-50;
+      } else {
+        pong.players[0].pos.y = event.y;
       };
+    })
 
-      callback();
-      this.reset();
-    }
+    canvas.addEventListener('click', event => { pong.start()})
 
-    collide(player, ball){
-      if(player.left < ball.right && player.right > ball.left && player.top < ball.bottom && player.bottom > ball.top){
-        const len = ball.vel.len;
-        ball.vel.x = -ball.vel.x;
-        console.log('vel x: ' + ball.vel.x);
-        ball.vel.y = -ball.vel.y;
-        console.log('vel y: ' + ball.vel.y);
-        ball.vel.y += 300 * (Math.random() - 0.5);
-        ball.vel.len = len * 1.05
-        console.log('len: ' + ball.vel.len);
-      }
-    }
-
-    drawRect(rectangle){
-      this._context.fillStyle = '#FFF';
-      this._context.fillRect(rectangle.left, rectangle.top,
-                   rectangle.size.x, rectangle.size.y)
-    }
-
-    draw(){
-      this._context.fillStyle = '#000';
-      this._context.fillRect(0,0,this._canvas.width, this._canvas.height)
-
-      this.drawRect(this.ball)
-      this.players.forEach( player => {
-        this.drawRect(player)
-      })
-    }
-
-    reset(){
-      this.ball.pos.x = this._canvas.width/2;
-      this.ball.pos.y = this._canvas.height/2;
-      this.ball.vel.x = 0;
-      this.ball.vel.y = 0;
-    }
-
-    start(){
-      if(this.ball.vel.x === 0 && this.ball.vel.y === 0){
-        this.ball.vel.x = 300 * (Math.random() > 0.5 ? 1: -1);
-        this.ball.vel.y = 300 * (Math.random() > 0.5 ? 1: -1);
-        this.ball.vel.len = 200;
-      }
-    }
-
-    update(dt){
-      this.ball.pos.x += this.ball.vel.x * dt;
-      this.ball.pos.y += this.ball.vel.y * dt;
-      // console.log(this.ball.pos.x, this.ball.pos.y);
-
-      if (this.ball.left < 0 || this.ball.right > this._canvas.width){
-        const playerId = this.ball.vel.x < 0 | 0
-        this.players[playerId].score++
-        this.reset()
-      }
-
-      if (this.ball.top < 0 || this.ball.bottom > this._canvas.height){
-        this.ball.vel.y = -this.ball.vel.y
-      }
-
-      this.players[1].pos.y = this.ball.pos.y
-      this.players.forEach( player => { this.collide(player, this.ball)})
-      this.draw();
-    }
-  }
-
-  const canvas = document.getElementById('pong');
-  const pong = new Pong(canvas);
-
-  canvas.addEventListener('mousemove', event => {
-    if (event.y < 50){
-      pong.players[0].pos.y = 50;
-    } else if ( event.y > canvas.height-50 ) {
-      pong.players[0].pos.y = canvas.height-50;
-    } else {
-      pong.players[0].pos.y = event.y;
-    };
   })
-
-  canvas.addEventListener('click', event => { pong.start()})
-
-})
+}
--- app/assets/javascripts/5-connect/connect.js
+++ app/assets/javascripts/5-connect/connect.js
@@ -1,264 +1,267 @@
-document.addEventListener("turbolinks:load", function(){
-  let wall = document.getElementById('wall');
-  let coren = new Game;
-  coren.cards.forEach(function(card, index) {
-    let cardElement = document.createElement('div');
-    cardElement.id = card.id;
-    cardElement.classList.add('card');
-    cardElement.innerText = card.text;
-    cardElement.addEventListener('click', function(event){
-      if(coren.selections.cards.length < 3){
-        coren.selectCard(event.target);
-      } else {
-        coren.selectCard(event.target);
-        if(coren.selections.cards.length === 4 && coren.areCardsSameGroup()){
-            coren.positionSortedCards();
-            setTimeout(function(){coren.resetSelectedCards(), 3000});
-        } else if (coren.selections.cards.length === 4 && !coren.areCardsSameGroup()){
-          setTimeout(function(){coren.resetSelectedCards(), 3000});
-        }
-      }
-    })
-    wall.appendChild(cardElement);
-  })
-  let checkButton = document.getElementById('checkButton');
-  checkButton.addEventListener('click', function(){
-    coren.checkAnswer();
+if(/days\/5/.test(window.location.href)){
+
+  document.addEventListener("turbolinks:load", function(){
+      let wall = document.getElementById('wall');
+      let coren = new Game;
+      coren.cards.forEach(function(card, index) {
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add('card');
+        cardElement.innerText = card.text;
+        cardElement.addEventListener('click', function(event){
+          if(coren.selections.cards.length < 3){
+            coren.selectCard(event.target);
+          } else {
+            coren.selectCard(event.target);
+            if(coren.selections.cards.length === 4 && coren.areCardsSameGroup()){
+                coren.positionSortedCards();
+                setTimeout(function(){coren.resetSelectedCards(), 3000});
+            } else if (coren.selections.cards.length === 4 && !coren.areCardsSameGroup()){
+              setTimeout(function(){coren.resetSelectedCards(), 3000});
+            }
+          }
+        })
+        wall.appendChild(cardElement);
+      })
+      let checkButton = document.getElementById('checkButton');
+      checkButton.addEventListener('click', function(){
+        coren.checkAnswer();
+      });
+
+      let skipButton = document.getElementById('skipButton');
+      skipButton.addEventListener('click', function(){
+        coren.skip();
+      });
   });
 
-  let skipButton = document.getElementById('skipButton');
-  skipButton.addEventListener('click', function(){
-    coren.skip();
-  });
-});
+  class Game {
+    constructor(){
+      this.answers = {
+        1: "vvv good Sunday evening my bois,vvv good evening mah bois,vvv good evening my bois,vvv good",
+        2: "hinge,early dating,dating app,dating chat,Hinge,chat,messages",
+        3: "new years day,January first,anniversary, new years,1st, 4th date, date 4",
+        4: "edgy dates,dates, we did",
 
-class Game {
-  constructor(){
-    this.answers = {
-      1: "vvv good Sunday evening my bois,vvv good evening mah bois,vvv good evening my bois,vvv good",
-      2: "hinge,early dating,dating app,dating chat,Hinge,chat,messages",
-      3: "new years day,January first,anniversary, new years,1st, 4th date, date 4",
-      4: "edgy dates,dates, we did",
-
-    };
-    this.currentRowToConnect = 1;
-    this.questionsSorted = new Set([]);
-    this.promptsByRow = {2:"Great! Now, row 2: ", 3:"Row 3: ", 4:"FINAL ROW 4 MAH BOIS:"};
-    this.answersByQuestion = {1:"Answer: vvv good (Sunday) evening mah bois", 2:"Answer: Things we talked about on Hinge", 3:"Answer: Things from our date on New Years Day", 4:"Answer: Edgy dates"};
-    this.sortedRowInfo = [{ row: 1, question:0}, { row: 2, question:0}, { row: 3, question:0}, { row: 4, question:0}];
-    this.isRowSorted = [false,false,false];
-    this.selections = {
-        cardSelected: false,
-        cards: []
       };
-    this.cards = [
-        { id: 1, text: "exceptionally, especially, exceedingly", group: 1 },
-        { id: 2, text: "cable cars", group: 2 },
-        { id: 3, text: "bicycle", group: 3 },
-        { id: 4, text: "axes", group: 4 },
-        { id: 5, text: "quality", group: 1 },
-        { id: 6, text: "Bruce Forsyth", group: 2 },
-        { id: 7, text: "walking tour", group: 3 },
-        { id: 8, text: "pharaoh", group: 4 },
-        { id: 9, text: "dusk", group: 1 },
-        { id: 10, text: "surgery on mice", group: 2 },
-        { id: 11, text: "brunch", group: 3 },
-        { id: 12, text: "yogi", group: 4 },
-        { id: 13, text: "dear gentlemen", group: 1 },
-        { id: 14, text: "harvesting personal data", group: 2 },
-        { id: 15, text: "champagne", group: 3 },
-        { id: 16, text: "pottery", group: 4 }
-      ];
+      this.currentRowToConnect = 1;
+      this.questionsSorted = new Set([]);
+      this.promptsByRow = {2:"Great! Now, row 2: ", 3:"Row 3: ", 4:"FINAL ROW 4 MAH BOIS:"};
+      this.answersByQuestion = {1:"Answer: vvv good (Sunday) evening mah bois", 2:"Answer: Things we talked about on Hinge", 3:"Answer: Things from our date on New Years Day", 4:"Answer: Edgy dates"};
+      this.sortedRowInfo = [{ row: 1, question:0}, { row: 2, question:0}, { row: 3, question:0}, { row: 4, question:0}];
+      this.isRowSorted = [false,false,false];
+      this.selections = {
+          cardSelected: false,
+          cards: []
+        };
+      this.cards = [
+          { id: 1, text: "exceptionally, especially, exceedingly", group: 1 },
+          { id: 2, text: "cable cars", group: 2 },
+          { id: 3, text: "bicycle", group: 3 },
+          { id: 4, text: "axes", group: 4 },
+          { id: 5, text: "quality", group: 1 },
+          { id: 6, text: "Bruce Forsyth", group: 2 },
+          { id: 7, text: "walking tour", group: 3 },
+          { id: 8, text: "pharaoh", group: 4 },
+          { id: 9, text: "dusk", group: 1 },
+          { id: 10, text: "surgery on mice", group: 2 },
+          { id: 11, text: "brunch", group: 3 },
+          { id: 12, text: "yogi", group: 4 },
+          { id: 13, text: "dear gentlemen", group: 1 },
+          { id: 14, text: "harvesting personal data", group: 2 },
+          { id: 15, text: "champagne", group: 3 },
+          { id: 16, text: "pottery", group: 4 }
+        ];
 
-  }
-
-  getCardPositionClasses(index){
-    let col, row;
-    col = (index%4)+1;
-    row = (Math.ceil((index-3)/4))+1;
-    return {
-      col: `col_${col}`,
-      row: `row_${row}`
     }
-  }
 
-  resetSelectedCards(){
-    // depend on HTML being set up
-    this.selections.cards.forEach(function(card){
-      let element = document.getElementById(card.id);
-      element.classList.remove('selected');
-
-    })
-    this.selections.cards = [];
-    this.selections.cardSelected = false;
-  }
-
-  isCardAlreadySelected(card){
-    if(this.selections.cards.length == 0){
-      return false
-    } else {
-      let searchForCard = this.selections.cards.filter(x => x.id == parseInt(card.id) );
-      return searchForCard.length > 0 ? true : false;
-    }
-  }
-
-  selectCard(card){
-    if(this.selections.cards.length < 4  && !this.isCardAlreadySelected(card)){
-      let selectedCardData = this.cards.filter(x => x.id == parseInt(card.id) )[0];
-      this.selections.cards.push(selectedCardData);
-    }
-    card.classList.add('selected');
-  }
-
-  areCardsSameGroup(){
-    let groups = []
-    this.selections.cards.forEach( card => {
-      groups.push(card.group);
-    })
-    let selectedSets = new Set(groups);
-    if(selectedSets.size === 1){
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  updateGameStats(freeRow){
-    if (!this.questionsSorted.has(this.selections.cards[0].group)){
-      this.questionsSorted.add(this.selections.cards[0].group);
-      this.sortedRowInfo[freeRow-1] = {row: freeRow, question: this.selections.cards[0].group};
-    } else {
-      //adding 4th question
-      for (let i=1; i <= 4; i++){
-        if (!this.questionsSorted.has(i)){
-          this.questionsSorted.add(i);
-          this.sortedRowInfo[freeRow-1] = {row: freeRow, question: i}
-        }
+    getCardPositionClasses(index){
+      let col, row;
+      col = (index%4)+1;
+      row = (Math.ceil((index-3)/4))+1;
+      return {
+        col: `col_${col}`,
+        row: `row_${row}`
       }
     }
-    this.isRowSorted[freeRow-1] = true;
-  }
 
-  positionSortedCards(){
-    let freeRow = (this.isRowSorted.indexOf(false)) + 1;
-    this.updateGameStats(freeRow);
-    if(freeRow > 0 && freeRow < 3){
-      this.selections.cards.forEach( card => {
+    resetSelectedCards(){
+      // depend on HTML being set up
+      this.selections.cards.forEach(function(card){
         let element = document.getElementById(card.id);
-        element.classList.add(`row_${freeRow}`);
         element.classList.remove('selected');
-        element.classList.add('correct');
+
       })
-    } else if (freeRow == 3) {
+      this.selections.cards = [];
+      this.selections.cardSelected = false;
+    }
+
+    isCardAlreadySelected(card){
+      if(this.selections.cards.length == 0){
+        return false
+      } else {
+        let searchForCard = this.selections.cards.filter(x => x.id == parseInt(card.id) );
+        return searchForCard.length > 0 ? true : false;
+      }
+    }
+
+    selectCard(card){
+      if(this.selections.cards.length < 4  && !this.isCardAlreadySelected(card)){
+        let selectedCardData = this.cards.filter(x => x.id == parseInt(card.id) )[0];
+        this.selections.cards.push(selectedCardData);
+      }
+      card.classList.add('selected');
+    }
+
+    areCardsSameGroup(){
+      let groups = []
       this.selections.cards.forEach( card => {
-        let element = document.getElementById(card.id);
-        element.classList.add(`row_${freeRow}`);
+        groups.push(card.group);
       })
-      this.updateGameStats(freeRow+1);
-      this.addRowClassLastFourCards();
-      let cards = document.getElementsByClassName('card')
+      let selectedSets = new Set(groups);
+      if(selectedSets.size === 1){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    updateGameStats(freeRow){
+      if (!this.questionsSorted.has(this.selections.cards[0].group)){
+        this.questionsSorted.add(this.selections.cards[0].group);
+        this.sortedRowInfo[freeRow-1] = {row: freeRow, question: this.selections.cards[0].group};
+      } else {
+        //adding 4th question
+        for (let i=1; i <= 4; i++){
+          if (!this.questionsSorted.has(i)){
+            this.questionsSorted.add(i);
+            this.sortedRowInfo[freeRow-1] = {row: freeRow, question: i}
+          }
+        }
+      }
+      this.isRowSorted[freeRow-1] = true;
+    }
+
+    positionSortedCards(){
+      let freeRow = (this.isRowSorted.indexOf(false)) + 1;
+      this.updateGameStats(freeRow);
+      if(freeRow > 0 && freeRow < 3){
+        this.selections.cards.forEach( card => {
+          let element = document.getElementById(card.id);
+          element.classList.add(`row_${freeRow}`);
+          element.classList.remove('selected');
+          element.classList.add('correct');
+        })
+      } else if (freeRow == 3) {
+        this.selections.cards.forEach( card => {
+          let element = document.getElementById(card.id);
+          element.classList.add(`row_${freeRow}`);
+        })
+        this.updateGameStats(freeRow+1);
+        this.addRowClassLastFourCards();
+        let cards = document.getElementsByClassName('card')
+        for(let i=0; i < cards.length; i++){
+          cards[i].classList.add('correct')
+        }
+        this.showTextInputField();
+        this.highlightRowToConnect();
+      }
+    }
+
+    addRowClassLastFourCards(){
+      let cards = document.getElementsByClassName('card');
+      let lastFour = [];
       for(let i=0; i < cards.length; i++){
-        cards[i].classList.add('correct')
+        if(!/row_/.test(cards[i].classList.value)){
+          lastFour.push(cards[i]);
+        }
       }
-      this.showTextInputField();
-      this.highlightRowToConnect();
+      lastFour.forEach(card => {
+        card.classList.add('row_4');
+      })
     }
-  }
 
-  addRowClassLastFourCards(){
-    let cards = document.getElementsByClassName('card');
-    let lastFour = [];
-    for(let i=0; i < cards.length; i++){
-      if(!/row_/.test(cards[i].classList.value)){
-        lastFour.push(cards[i]);
+    showTextInputField(){
+      document.getElementById('connect').style.visibility = 'visible';
+    }
+
+    getTextInput(){
+      let input = document.getElementById('input').value;
+      return input;
+    }
+
+    clearTextInput(){
+      let input = document.getElementById('input');
+      input.value = "";
+    }
+
+    removeHighlightRowToConnect(){
+      let cardsConnect = document.getElementsByClassName('row_' + this.currentRowToConnect);
+      for(let i=0; i < cardsConnect.length; i++){
+        cardsConnect[i].classList.remove('highlight');
       }
     }
-    lastFour.forEach(card => {
-      card.classList.add('row_4');
-    })
-  }
 
-  showTextInputField(){
-    document.getElementById('connect').style.visibility = 'visible';
-  }
-
-  getTextInput(){
-    let input = document.getElementById('input').value;
-    return input;
-  }
-
-  clearTextInput(){
-    let input = document.getElementById('input');
-    input.value = "";
-  }
-
-  removeHighlightRowToConnect(){
-    let cardsConnect = document.getElementsByClassName('row_' + this.currentRowToConnect);
-    for(let i=0; i < cardsConnect.length; i++){
-      cardsConnect[i].classList.remove('highlight');
+    highlightRowToConnect(){
+      let cardsConnect = document.getElementsByClassName('row_' + this.currentRowToConnect);
+      for(let i=0; i < cardsConnect.length; i++){
+        cardsConnect[i].classList.add('highlight');
+      }
     }
-  }
 
-  highlightRowToConnect(){
-    let cardsConnect = document.getElementsByClassName('row_' + this.currentRowToConnect);
-    for(let i=0; i < cardsConnect.length; i++){
-      cardsConnect[i].classList.add('highlight');
+    updatePromptText(text){
+      let prompt = document.getElementById('prompt-text');
+      prompt.innerText = text;
     }
-  }
 
-  updatePromptText(text){
-    let prompt = document.getElementById('prompt-text');
-    prompt.innerText = text;
-  }
+    updateAnswerText(text){
+      let answer = document.getElementById('answer-text');
+      answer.innerText = text;
+    }
 
-  updateAnswerText(text){
-    let answer = document.getElementById('answer-text');
-    answer.innerText = text;
-  }
+    checkAnswer(){
+      let row = this.currentRowToConnect;
+      let question = this.sortedRowInfo[row-1].question;
+      let answers = this.answers[question].split(',');
+      let submission = this.getTextInput();
+      let correct = false;
+      answers.forEach( ans => {
+        if ((new RegExp(ans)).test(submission)){
+          correct = true;
+        };
+      })
 
-  checkAnswer(){
-    let row = this.currentRowToConnect;
-    let question = this.sortedRowInfo[row-1].question;
-    let answers = this.answers[question].split(',');
-    let submission = this.getTextInput();
-    let correct = false;
-    answers.forEach( ans => {
-      if ((new RegExp(ans)).test(submission)){
-        correct = true;
-      };
-    })
+      if(correct){
+        this.removeHighlightRowToConnect();
+        this.currentRowToConnect = row + 1;
+        if (this.currentRowToConnect < 5){
+          this.highlightRowToConnect();
+          this.updatePromptText(this.promptsByRow[this.currentRowToConnect]);
+        } else {
+          this.updatePromptText("You win!")
+        }
+        this.updateAnswerText(this.answersByQuestion[question]);
+        this.clearTextInput();
+      } else {
+        this.updateAnswerText("Nuh-uh, incorrect");
+      }
+    }
 
-    if(correct){
+    skip(){
+      let row = this.currentRowToConnect;
+      let question = this.sortedRowInfo[row-1].question;
       this.removeHighlightRowToConnect();
       this.currentRowToConnect = row + 1;
       if (this.currentRowToConnect < 5){
         this.highlightRowToConnect();
         this.updatePromptText(this.promptsByRow[this.currentRowToConnect]);
+        this.updateAnswerText(this.answersByQuestion[question]);
+        this.clearTextInput();
       } else {
-        this.updatePromptText("You win!")
+        this.updatePromptText("You win!");
+        this.updateAnswerText(this.answersByQuestion[question]);
+        this.clearTextInput();
       }
-      this.updateAnswerText(this.answersByQuestion[question]);
-      this.clearTextInput();
-    } else {
-      this.updateAnswerText("Nuh-uh, incorrect");
     }
-  }
 
-  skip(){
-    let row = this.currentRowToConnect;
-    let question = this.sortedRowInfo[row-1].question;
-    this.removeHighlightRowToConnect();
-    this.currentRowToConnect = row + 1;
-    if (this.currentRowToConnect < 5){
-      this.highlightRowToConnect();
-      this.updatePromptText(this.promptsByRow[this.currentRowToConnect]);
-      this.updateAnswerText(this.answersByQuestion[question]);
-      this.clearTextInput();
-    } else {
-      this.updatePromptText("You win!");
-      this.updateAnswerText(this.answersByQuestion[question]);
-      this.clearTextInput();
-    }
   }
-
 }
