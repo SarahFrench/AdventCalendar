e90256f2bb131b9e98502f2d979165e58475043b
Sarah French <sarah@sarahfrench.co.uk>
2019-10-23 00:19:04 +0100

    Starting to try looping an audio file playback based on number of clicks

--- app/assets/javascripts/7-rapid/rapid.js
+++ app/assets/javascripts/7-rapid/rapid.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
   const TEN_SECONDS_IN_MS = 10000;
+
   let game = {
     firstClick: 0,
-    numberOfClicks: 0
+    numberOfClicks: 0,
+    audioPlaying: false
   }
   let button = document.getElementById('rapid-click');
 
@@ -15,8 +17,15 @@ document.addEventListener('DOMContentLoaded', function(){
         console.log("click within 10 seconds of first");
         game.numberOfClicks++;
       } else {
-        console.log("click >10 seconds after first");
-        console.log(game);
+        game.audioPlaying = true;
+        let audio = document.getElementById('audio');
+        while(game.audioPlaying){
+          for(let i=0; i < game.numberOfClicks; i++){
+            console.log(i);
+            setTimeout(function(){console.log(audio.currentTime);audio.currentTime = 4; audio.play()}, 2000)
+          }
+          game.audioPlaying = false;
+        }
       }
     }
   })
