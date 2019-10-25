c7a7fc929bd6eaec769b597be2ec5e58908542e8
Sarah French <sarah@sarahfrench.co.uk>
2019-10-25 13:31:53 +0100

    Audio section repeats, starts at `delay` and has len `repeatDuration` Can play audio file up to the delay timestamp, then section following that timestamp that has duration `repeatDuration` will repeat a number of times based on click numbers. On the last loop the audio plays out to the end of the file

--- app/assets/javascripts/7-rapid/rapid.js
+++ app/assets/javascripts/7-rapid/rapid.js
@@ -1,32 +1,46 @@
 document.addEventListener('DOMContentLoaded', function(){
   const TEN_SECONDS_IN_MS = 10000;
+  const ONE_SECONDS_IN_MS = 1000;
 
   let game = {
     firstClick: 0,
-    numberOfClicks: 0,
-    audioPlaying: false
+    numberOfClicks: 0
   }
   let button = document.getElementById('rapid-click');
 
   button.addEventListener('click', function(event){
+    let audio = document.getElementById('audio');
     if(game.firstClick === 0){
       game.firstClick = event.timeStamp;
       console.log("first click at " + game.firstClick)
     } else {
-      if(event.timeStamp - game.firstClick < TEN_SECONDS_IN_MS){
+      if(event.timeStamp - game.firstClick < ONE_SECONDS_IN_MS){
         console.log("click within 10 seconds of first");
         game.numberOfClicks++;
-      } else {
-        game.audioPlaying = true;
-        let audio = document.getElementById('audio');
-        while(game.audioPlaying){
-          for(let i=0; i < game.numberOfClicks; i++){
-            console.log(i);
-            setTimeout(function(){console.log(audio.currentTime);audio.currentTime = 3; audio.play()}, 1000*i)
-          }
-          game.audioPlaying = false;
-        }
+      } else if (audio.currentTime === 0) {
+        console.log("curr Time =" + audio.currentTime);
+        playAudioWithDelayedRepeat(audio, 1000, 500, game.numberOfClicks);
       }
     }
   })
 })
+
+function playAudioWithDelayedRepeat(audio, delay, repeatDuration, numberRepeats){
+  let audioPlaying = true
+  let callback = function(){
+    while(audioPlaying){
+      for(let i=0; i < numberRepeats; i++){
+        console.log(i);
+        setTimeout(function(){
+          console.log("loop " + i + ": "+ audio.currentTime);
+          audio.currentTime = 0.9;
+          audio.play();
+        }, (repeatDuration*i));
+      }
+      audioPlaying = false;
+    }
+  }
+
+  audio.play();
+  setTimeout(callback, delay);
+}
