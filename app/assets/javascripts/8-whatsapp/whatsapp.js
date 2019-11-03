9024767acdcaaac26c9c7d1e783a9f927b3278b4
Sarah French <sarah@sarahfrench.co.uk>
2019-11-03 17:34:33 +0000

    JS creates charts using GET responses if user answers correctly. Made on the fly

--- app/assets/javascripts/8-whatsapp/whatsapp.js
+++ app/assets/javascripts/8-whatsapp/whatsapp.js
@@ -1,5 +1,19 @@
-document.addEventListener('DOMContentLoaded', function(){
-  var ctx = document.getElementById('myChart').getContext('2d');
+function convertStatsToArray(stats){
+  stats = JSON.parse(stats);
+  let months = Object.keys(stats);
+  let array = [];
+  months.forEach( month => {
+    array[month] = stats[month];
+  })
+  return array;
+}
+
+function addLineChart(word, stats){
+  let sarahData = convertStatsToArray(stats.sarah_months_frequency)
+  let hugoData = convertStatsToArray(stats.hugo_months_frequency)
+  console.log(sarahData);
+  console.log(hugoData);
+  var ctx = document.getElementById(`chart-${word}`).getContext('2d');
   var chart = new Chart(ctx, {
       // The type of chart we want to create
       type: 'line',
@@ -9,14 +23,14 @@ document.addEventListener('DOMContentLoaded', function(){
           labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November'],
           datasets: [{
               label: 'Sarah',
-              backgroundColor: 'rgba(255, 99, 132, 0.5)',
-              borderColor: 'rgb(255, 99, 132)',
-              data: [119,70,59,25,28,22,16,16,8,7,0,50]
+              backgroundColor: 'rgba(255,188,88,0.5)',
+              borderColor: 'rgb(255,188,88)',
+              data: sarahData
           },{
               label: 'Hugo',
-              backgroundColor: 'rgba(99, 255, 132, 0.5)',
-              borderColor: 'rgb(99, 255, 132)',
-              data: [58,34,20,7,8,10,3,3,2,0,0,24]
+              backgroundColor: 'rgba(88,155,255, 0.5)',
+              borderColor: 'rgb(88,155,255)',
+              data: hugoData
           }]
       },
 
@@ -27,40 +41,60 @@ document.addEventListener('DOMContentLoaded', function(){
   });
   chart.canvas.parentNode.style.height = '200px';
   chart.canvas.parentNode.style.width = '500px';
-});
+};
 
 document.addEventListener('DOMContentLoaded', function(event){
   const buttons = document.getElementsByClassName('answer-button');
   for(let i=0; i < buttons.length; i++){
     buttons[i].addEventListener('click', function(){
-      showAnswer(buttons[i]);
+      processAnswer(buttons[i]);
     })
   }
-
-
 })
 
 
-function showAnswer(element){
+function processAnswer(element){
+  //get the query param from button ID
   const word = element.id.replace(/^\w+-/, "").replace(/-button$/, "");
+  //get the choice of sarah or hugo from button ID
+  const usersGuess = element.id.match(/^\w+/)[0];
   if(!document.getElementById(`answer-${word}`)){
     const stats = getStats(word);
     stats.then( stats => {
       const percentages = calcPercentages(stats);
-      answer = percentages.hugo > percentages.sarah ? "hugo" : "sarah";
-      console.log(percentages);
-      showAnswerOnPage(element, answer, word)
+      correctAnswer = percentages.hugo > percentages.sarah ? "hugo" : "sarah";
+      let wasUserCorrect = (correctAnswer === usersGuess)
+      showAnswerOnPage(element, correctAnswer, word, percentages, wasUserCorrect);
+      if(wasUserCorrect){
+        addLineChart(word, stats);
+        showChart(word, stats);
+      }
     })
   } else {
     console.log("clicked already");
   }
 }
 
-function showAnswerOnPage(element, answer, word){
+function showAnswerOnPage(element, correctAnswer, word, percentages, wasUserCorrect){
+  correctAnswer = correctAnswer[0].toUpperCase() + correctAnswer.slice(1)
   let el = document.createElement('p');
   el.id = `answer-${word}`
-  el.innerText = `The answer is ${answer}`;
+  let responseText = "";
+  if(wasUserCorrect){
+    el.classList.add('correct');
+    responseText = "Correct! ";
+  } else {
+    el.classList.add('incorrect');
+    responseText = "Incorrect... ";
+  }
+  el.innerHTML = `<p> ${responseText} The answer is ${correctAnswer} </p>`;
+  el.innerHTML += `<p> Sarah: ${formatPercentageString(percentages.sarah)} Hugo: ${formatPercentageString(percentages.hugo)} </p>`;
   element.parentNode.appendChild(el);
+}
+
+function formatPercentageString(percentage){
+  percentage *= 100;
+  return `${percentage.toPrecision(4)}%`
 }
 
 function calcPercentages(stats){
@@ -75,4 +109,8 @@ async function getStats(word){
       method: 'GET'
     });
   return await response.json();
+}
+
+function showChart(word){
+  document.getElementById(`chart-${word}`).style.display = "block"
 }
