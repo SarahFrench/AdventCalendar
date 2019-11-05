37910b39816d828240833b96ce026ab760e52388
Sarah French <sarah@sarahfrench.co.uk>
2019-11-05 21:22:45 +0000

    route to put data in prod DB

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/about', to: 'home#about'
   resources :challenges, only: [:show], path: '/days'
 
+  get '/initialise-whatsapp-stats', to: 'whatsapp_stats#init'
   get '/delete-click-scores', to: 'click_scores#destroy_all'
   get '/delete-local-storage', to: 'home#delete-local-storage'
 end
