a83f95d55bf79b80c9696cc69406e6312fb8e43e
Sarah French <sarah@sarahfrench.co.uk>
2019-11-17 18:40:15 +0000

    Able to do better geolocation when use MapBox API for location info. NOTE: excluded API key variable `key`from push

--- app/assets/javascripts/19-geolocation/geolocation.js
+++ app/assets/javascripts/19-geolocation/geolocation.js
@@ -1,4 +1,4 @@
-function getLocation(){
+function getLatLong(){
   return new Promise((resolve, reject)=>{
     navigator.geolocation.getCurrentPosition(function(position){
       resolve(position);
@@ -6,57 +6,78 @@ function getLocation(){
   });
 }
 
+
+function getLocationInfo(lat, long){
+  return new Promise((resolve, reject)=>{
+    const response = fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${long},${lat}.json?access_token=${key}`, {
+        method: 'GET'
+    })
+    resolve(response);
+  });
+}
+
+function hasLeftLondon(lat, long){
+  return( lat < 51.447040 && long < -0.666881);
+}
+
+function isNearLooe(lat, long){
+  return( lat < 50.403443 && long < -4.396514);
+}
+
+function decideResponseSentence(data){
+  let locationString = data.data.features[0].text;
+  console.log("DATA");
+  console.log(data.data);
+
+
+  if(data.leftLondon && !data.nearLooe){
+    return "You've left London? Finally!"
+  } else if (data.nearLooe) {
+    return "You've arrived!"
+  } else if (locationString === "Kentish Town Road"){
+    return "You're home, good. I can keep an eye on you with the secret cameras.";
+  } else if (locationString === "Avondale Road"){
+    return "You're down in Croydon? I miss you but...";
+  } else {
+    return `What are you doing in ${locationString}? Without me?? :( `
+  }
+}
+
+
 //Promise
-let locationCoords = getLocation();
+let coordinates = getLatLong();
 
+coordinates.then( position => {
+  locationInfo = getLocationInfo(position.coords.latitude, position.coords.longitude);
+  return locationInfo;
+}).then( data => {
+  data = data.json();
+  return data
+}).then( data => {
+  let lat = data.query[1];
+  let long = data.query[0];
 
-locationCoords
-  .then(position => {
-    let string = "";
-    if(
-      (position.coords.latitude > 51.545341 && position.coords.latitude < 51.550892)
-      &&
-      (position.coords.longitude > -0.143788 && position.coords.longitude < -0.139293)
-    ){
-      string = "Are you in Kentish Town?";
-    } else if(
-        (position.coords.latitude > 51.352095 && position.coords.latitude < 51.357107)
-        &&
-        (position.coords.longitude > -0.103072 && position.coords.longitude < -0.094135)
-      ){
-        string = "Are you in Croydon?";
-      } else if(
-          (position.coords.latitude > 51.500019 && position.coords.latitude < 51.507345)
-          &&
-          (position.coords.longitude > -0.090555 && position.coords.longitude < -0.079676)
-        ){
-          string = "Are you travelling through London Bridge?";
-        } else if(
-            (position.coords.latitude > 50.332942 && position.coords.latitude < 50.373735 )
-            &&
-            (position.coords.longitude > -4.499282 && position.coords.longitude < -4.397229)
-          ){
-            string = "Are you in Looe?";
-          } else {
-          string = "I don't know where you are right now, but...";
-        }
+  return {
+    data: data,
+    leftLondon: hasLeftLondon(lat, long),
+    nearLooe: isNearLooe(lat, long)
+  };
+}).then( data => {
+  let message = decideResponseSentence(data);
+  let locationText = document.getElementById('location');
+  locationText.innerText = message;
+  locationText.style.visibility = "visible";
 
-      return string;
-  })
-  .then(message => {
-    console.log(location);
-    let locationText = document.getElementById('location');
-    locationText.innerText = message;
-    locationText.style.visibility = "visible";
+  //creepy second message added after delay
+  setTimeout(function(){
+    document.getElementById('message').style.visibility = "visible";
+  }, 2000);
 
-    setTimeout(function(){
-      document.getElementById('message').style.visibility = "visible";
-    }, 2000);
-  })
-  .catch( error => {
-    setTimeout(function(){
-      let messageText = document.getElementById('message');
-      messageText.innerText = "I'm always watching.";
-      messageText.style.visibility = "visible";
-    }, 2000);
-  })
+}).catch( error => {
+  //only show second message
+  setTimeout(function(){
+    let messageText = document.getElementById('message');
+    messageText.innerText = "I'm always watching.";
+    messageText.style.visibility = "visible";
+  }, 2000);
+})
