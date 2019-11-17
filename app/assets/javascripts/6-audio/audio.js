359589ae0fe4aac9f138d274a9b4577507dc2e61
Sarah French <sarah@sarahfrench.co.uk>
2019-11-17 10:37:28 +0000

    Audio quiz finished

--- app/assets/javascripts/6-audio/audio.js
+++ app/assets/javascripts/6-audio/audio.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
   let button = document.getElementById('audio-button');
   let audio = document.getElementById('audio');
+  let iconEasterEgg = document.getElementById('easter-egg');
+  let audioEasterEgg = document.getElementById('audio-easteregg');
+  let audioEasterEggContainer = document.getElementById('easter-egg-container');
   let form = document.getElementById('audio-answer-form')
 
   button.addEventListener('click', function(){
@@ -8,6 +11,14 @@ document.addEventListener('DOMContentLoaded', function(){
     setTimeout(function(){
       form.style.visibility = "visible";
     }, (audio.duration*1000));
+
+  })
+
+  iconEasterEgg.addEventListener('click', function(){
+    audioEasterEgg.play();
+    setTimeout(function(){
+      audioEasterEggContainer.innerHTML = "<small class=\"audio__grey\">You found the Easter egg! Sound familiar?</small>"
+    }, (audioEasterEgg.duration*1000));
 
   })
 
