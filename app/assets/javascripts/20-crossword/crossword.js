1bfb22eb22929594bb5a854e4d48ab97b3146458
Sarah French <sarah@sarahfrench.co.uk>
2019-11-21 22:27:45 +0000

    Crossword is 100% javascript now

--- app/assets/javascripts/20-crossword/crossword.js
+++ app/assets/javascripts/20-crossword/crossword.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
+  loadAnswers();
+
   let clues = document.getElementsByClassName('clue');
   for(let i=0; i < clues.length; i++){
     clues[i].addEventListener('click', function(event){
       removeHighlight();
       let group = event.target.id.replace("clue-", "");
-
       addHighlight(group);
     })
   }
@@ -18,17 +19,33 @@ document.addEventListener('DOMContentLoaded', function(){
     let boxes = document.getElementsByClassName(group)
 
     for(let i=0; i < boxes.length; i++){
-      boxes[i].classList.add('highlight');
+      boxes[i].classList.add('crossword__highlight');
     }
   }
 
   function removeHighlight(){
-    let boxes = document.getElementsByClassName('highlight');
+    let boxes = document.getElementsByClassName('crossword__highlight');
     while (boxes[0]) {
-    boxes[0].classList.remove('highlight');
+      boxes[0].classList.remove('crossword__highlight');
     }
   }
 
+})
+
+function clearAnswers(){
+  saveAnswers('[]');
+  fillInBoxes({});
+}
+
+function removeErrorHighlight(){
+  let boxes = document.getElementsByClassName('crossword__incorrect');
+  while (boxes[0]) {
+    boxes[0].classList.remove('crossword__incorrect');
+  }
+}
+
+document.addEventListener('click', function(){
+  removeErrorHighlight();
 })
 
 
@@ -52,18 +69,53 @@ document.addEventListener('DOMContentLoaded', function(){
 
     }).then( correctLetters => {
 
-      for(let i = 1; i < 95; i++){
-        let answer = correctLetters[`letter-${i}`];
-        let el = document.querySelector(`input[id=letter-${i}]`);
-        el.value = typeof answer != 'undefined' ? answer : "";
+      saveAnswers(correctLetters)
+      fillInBoxes(correctLetters)
+      if(isCompleted(correctLetters)){
+        changeMessage("Yay, you've completed the crossword! 100% correct!")
+      } else {
+        changeMessage("I've removed all the incorrect letters from the board. Click anywhere to remove the error highlight")
       }
-
-      console.log(correctLetters);
     })
 
 
   })
 })
+
+function isCompleted(correctLetters){
+  return Object.keys(correctLetters).length === 94
+}
+
+function changeMessage(message){
+  let el = document.getElementsByClassName('crossword__message')[0];
+  el.innerText = message;
+}
+
+function fillInBoxes(object){
+  for(let i = 1; i < 95; i++){
+    let answer = object[`letter-${i}`];
+    let el = document.querySelector(`input[id=letter-${i}]`);
+    if (answer === undefined){
+      //wipe letter
+      el.value = "";
+      el.classList.add('crossword__incorrect')
+    } else {
+      //fill in correct letter
+      el.value = answer;
+    }
+  }
+}
+
+function saveAnswers(json){
+  window.localStorage.setItem("advent-crossword", JSON.stringify(json))
+}
+
+function loadAnswers(){
+  if(localStorage['advent-crossword'] != undefined){
+    let answers = JSON.parse(window.localStorage["advent-crossword"]);
+    fillInBoxes(answers);
+  }
+}
 
 function getLetters(){
   const inputs = document.querySelectorAll('input[id^=letter]')
