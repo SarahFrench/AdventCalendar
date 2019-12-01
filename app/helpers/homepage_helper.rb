c36daae32f2f166becee8e342d2c9797bc863989
Sarah French <sarah@sarahfrench.co.uk>
2019-12-01 00:18:18 +0000

    Make the side menu have full 25 links

--- app/helpers/homepage_helper.rb
+++ app/helpers/homepage_helper.rb
@@ -18,16 +18,8 @@ module HomepageHelper
 
   def menu_links_html
     html = ""
-    if Time.now.year == 2019
-      for i in (1..Time.now.day)
-        if i < 26
-          html += "<a href=\"/days/#{i}\" class=\"menu-link pad-left\" >Day #{i}</a>"
-        end
-      end
-    else
-      for i in (1..25)
-        html += "<a href=\"/days/#{i}\" class=\"menu-link pad-left\" >Day #{i}</a>"
-      end
+    for i in (1..25)
+      html += "<a href=\"/days/#{i}\" class=\"menu-link pad-left\" >Day #{i}</a>"
     end
     html.html_safe
   end
